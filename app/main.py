7a868fcdaa54734501b0b3f98a0563fe939585dc
noname2048 <sungwook.csw@gmail.com>
2022-04-29 22:53:29 +0900

    feat: ready to import oauth2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request, status
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from app.exceptions import StoryException
 from app.db import models
 from app.db.database import engine
@@ -12,6 +13,11 @@ app = FastAPI()
 app.include_router(article.router)
 app.include_router(product.router)
 app.include_router(user.router)
+
+
+app.add_middleware(
+    CORSMiddleware, allowed_origisn=["*"], allowed_headers=["*"], allowed_methods=["*"]
+)
 
 
 @app.get("/")
--- app/router/article.py
+++ app/router/article.py
@@ -3,6 +3,8 @@ from app.schemas import ArticleBase, ArticleDisplay
 from app.db.database import get_db
 from app.db import db_article
 from sqlalchemy.orm.session import Session
+from app.auth.oauth2 import oauth2_schema
+
 
 router = APIRouter(prefix="/article", tags=["article"])
 
@@ -15,5 +17,7 @@ async def create_article(request: ArticleBase, db: Session = Depends(get_db)):
 
 # Get specific article
 @router.get("/{id}", response_model=ArticleDisplay)
-async def get_article(id: int, db: Session = Depends(get_db)):
+async def get_article(
+    id: int, db: Session = Depends(get_db), token: str = Depends(oauth2_schema)
+):
     return db_article.get_article(int, db)
