4f3df9f83bacdfbc00e9b875ea99a41105f5489c
noname2048 <sungwook.csw@gmail.com>
2022-05-01 23:35:04 +0900

    feat: add more test

--- app/test_main.py
+++ app/test_main.py
@@ -1,8 +1,39 @@
+from os import access
 from fastapi.testclient import TestClient
-from main import app
+from app.main import app
+
+client = TestClient(app)
 
 
 def test_get_all_products():
-    client = TestClient(app)
     response = client.get("/product/all")
     assert response.status_code == 404
+
+
+def test_auth_error():
+    response = client.post("/token", data={"username": "", "password": ""})
+    access_token = response.json().get("access_token")
+    assert access_token == None
+    message = response.json().get("detail")[0].get("msg")
+    assert message == "field required"
+
+
+def test_auth_success():
+    response = client.post("/token", data={"username": "cat", "password": "cat"})
+    access_token = response.json().get("access_token")
+    assert access_token
+
+
+def test_post_article():
+    auth = client.post("/token", data={"username": "", "password": ""})
+    access_token = auth.json().get("access_token")
+    assert access_token
+
+    response = client.post(
+        "/article/",
+        json={"title": "Meow", "content": "cow", "published": True, "creator_id": 1},
+        headers={"Authorization": "bearer " + access_token},
+    )
+
+    assert response.status_code == 200
+    assert response.json().get("title") == "Meow"
