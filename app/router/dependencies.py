106d12f5a5a48da7165b139ea83805d76cb26dab
noname2048 <sungwook.csw@gmail.com>
2022-05-04 21:00:46 +0900

    feat: add class dependencies, for user

--- app/router/dependencies.py
+++ app/router/dependencies.py
@@ -18,3 +18,18 @@ def get_items(separator: str = "--", headers=Depends(convert_headers)):
 @router.post("/")
 def create_item(headers=Depends(convert_headers)):
     return {"result": "new item created", "headers": headers}
+
+
+class Account:
+    def __init__(self, name: str, email: str):
+        self.name = name
+        self.email = email
+
+
+@router.post("/user")
+def create_user(
+    name: str, email: str, password: str, account: Account = Depends(Account)
+):
+    # account - perform wether account operations
+    return {"name": account.name, "email": account.email}
+    pass
