791a84d173b87fd0417f54cec6d227969255b4e3
noname2048 <sungwook.csw@gmail.com>
2022-05-06 23:13:53 +0900

    feat: add hashing script

--- insta/db/db_user.py
+++ insta/db/db_user.py
@@ -1,13 +1,14 @@
 from insta.schemas import UserBase
 from insta.db.models import DbUser
 from sqlalchemy.orm import Session
+from insta.db.hashing import Hash
 
 
 def create_user(db: Session, request: UserBase):
     new_user = DbUser(
         username=request.username,
         email=request.email,
-        password=request.password,
+        password=Hash.bcrypt(request.password),
     )
     db.add(new_user)
     db.commit()
